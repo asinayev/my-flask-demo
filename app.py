9191e45b02f4390013a0c84875c943d2e9849888
Aleksandr Sinayev <asinayev@gmail.com>
2016-08-31 13:33:09 -0400

    consolidate

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-#from makeplot import makeplot	
+
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, output_file, show
+
+def makeplot(ticker):
+	stockdat = pd.read_csv("https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?start_date=2016-07-01&end_date=2016-08-01" % ticker)
+	output_file("templates/lines.html")
+	p = figure(title='Stocks',x_axis_label='Date', y_axis_label='Closing Price', 
+				x_axis_type="datetime")
+	dates = stockdat['Date'].astype('datetime64')
+	p.line(dates, stockdat['Close'], legend="Price", line_width=2)
+	show(p)
 
 app = Flask(__name__)
 
